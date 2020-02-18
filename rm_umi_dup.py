cb730fda1fe306a403c93607d278fe2f68e91304
tzhu-bio <54537420+tzhu-bio@users.noreply.github.com>
2020-02-18 17:32:22 +0800

    Create rm_umi_dup.py
    Removing PCR duplicates with mapping coordinates and UMI information.

---
+++ rm_umi_dup.py
@@ -0,0 +1,23 @@
+import pysam
+import os,re
+def rm_umidup_bam(bam_file,bam_out_folder):
+    umi_type = {6:"A1",13:"A5",20:"A6",25:"A7"}
+    sample_name = re.sub("_mapped_paired.bam","",os.path.basename(bam_file))
+    print(sample_name)
+    umi_id_dict={}
+    path_out = "%s/%s_rm_umidup.bam"%(bam_out_folder,sample_name)
+    infile = pysam.AlignmentFile(bam_file, "rb")
+    outfile = pysam.AlignmentFile(path_out, "wb", template=infile)
+    for read in infile:
+        umi = read.qname.split(':')[7]
+        if len(umi) in umi_type:
+            umi_id = "_".join([umi,read.reference_name,str(read.pos),str(read.tlen)])
+            if umi_id not in umi_id_dict:
+                umi_id_dict[umi_id] = 1
+                outfile.write(read)
+            else:
+                umi_id_dict[umi_id] += 1
+
+with open("~/umi_atac_seq_data/mapped_paired_bam_list","r") as mapped_paired_bam:
+    for i in mapped_paired_bam:
+        rm_umidup_bam(i.strip(),"~/umi_atac_seq_data/rm_umi_dup")
