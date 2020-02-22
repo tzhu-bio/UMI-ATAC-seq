df4f4ef0ec8d7bcce683d1ae33a9fab5aedb8f36
tzhu-bio <54537420+tzhu-bio@users.noreply.github.com>
2020-02-22 16:19:04 +0800

    Update rename_fastq_read2_header.py

--- rename_fastq_read2_header.py
+++ rename_fastq_read2_header.py
@@ -1,25 +1,26 @@
 import gzip
-# input your fastq read1 file 
-f1 = gzip.open("/public/home/tzhu/umi_raw_data/test/C019_fastq_read1.gz", "rt",encoding='utf-8')
-# input your fastq read2 file
-f2 =gzip.open('/public/home/tzhu/umi_raw_data/C019_fastq_read2.gz',"rt",encoding='utf-8')
-f3=open('/public/home/tzhu/umi_raw_data/C019_fq_r2_test','w') #file you want to write the result
-x=0
-y=0
-umi=[]
-lst_read2_line=[]
-lst_read2=[]
-for line in f1:
-    if line.startswith('@'):
-        l1_umi=line.strip().split(' ')[0]
-        umi.append(l1_umi) 
-for line in f2:
-    lst_read2_line.append(line)
-    if line.startswith('@'):
-        l2=line.strip().split(' ')[1]
-        lst_read2.append(l2)
-for i in range(len(umi)):        
-    f3.write(umi[i]+ ' ' +lst_read2[x]+'\n'+lst_read2_line[y+1]+lst_read2_line[y+2]+lst_read2_line[y+3])
-    x+=1
-    y+=4
-f3.close()
+import os,re
+def rename_fastq_read2_header(umi_fastq_read1,fastq_read2,umi_fastq_read2_folder):
+    f1 = gzip.open(umi_fastq_read1,"rt",encoding='utf-8')
+    f2 = gzip.open(fastq_read2,"rt",encoding='utf-8')
+    sample_name = re.sub(".gz","",os.path.basename(fastq_read2))
+    path_out = "%s/%s_umi_read2"%(umi_fastq_read2_folder,sample_name)
+    f3 = gzip.open(path_out,'wb') #file you want to write the result
+    y=0
+    umi=[]
+    lst_read2_line=[]
+    lst_read2=[]
+    for line in f1:
+        if line.startswith('@'):
+            l1_umi=line.strip().split(' ')[0]
+            umi.append(l1_umi) 
+    for line in f2:
+        lst_read2_line.append(line)
+        if line.startswith('@'):
+            l2=line.strip().split(' ')[1]
+            lst_read2.append(l2)
+    for i in range(len(umi)):        
+        f3.write(umi[i].encode()+ ' '.encode() +lst_read2[i].encode()+'\n'.encode()+lst_read2_line[y+1].encode()+lst_read2_line[y+2].encode()+lst_read2_line[y+3].encode())
+        y+=4
+    f3.close()
+    print('Done')
