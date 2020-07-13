7866537a1faa6dd8509517e2f55b8176dee5c0cb
Tao Zhu <54537420+tzhu-bio@users.noreply.github.com>
2020-07-13 10:52:54 +0800

    Update extract_umi.py

--- extract_umi.py
+++ extract_umi.py
@@ -10,17 +10,17 @@ me_1 = re.compile("AGATG")
 me_7 = re.compile("GTATA")
 me_15 = re.compile("GACAG")
 file_in = gzip.open(args.infile, "rt",encoding='utf-8')
-UMI_out = gzip.open(args.outfile,'wb')
+UMI_out = gzip.open(args.outfile,'wt')
 print('Extract UMIs from ' + os.path.basename(args.infile)+'...')
 for (title,sequence,quantily) in FastqGeneralIterator(file_in):
     me_1_pos = me_1.search(sequence)
     me_7_pos = me_7.search(sequence)
     me_15_pos = me_15.search(sequence)
     if me_1_pos and me_7_pos and (me_7_pos.start() - me_1_pos.start() == 6):
-        UMI_out.write('@'.encode()+title.strip().split(' ')[0].encode()+'_UMI:'.encode()+sequence[:6].encode()+' '.encode()+title.strip().split(' ')[1].encode()+'\n'.encode()+sequence[(me_1_pos.start()+19):].encode()+'\n'.encode()+'+'.encode()+'\n'.encode()+quantily[(me_1_pos.start()+19):].encode()+'\n'.encode())
+        UMI_out.write('@'+title.strip().split(' ')[0]+'_UMI:'+sequence[:6]+' '+title.strip().split(' ')[1]+'\n'+sequence[(me_1_pos.start()+19):]+'\n'+'+'+'\n'+quantily[(me_1_pos.start()+19):]+'\n')
     elif me_1_pos and me_15_pos and (me_15_pos.start() - me_1_pos.start() == 14):
-        UMI_out.write('@'.encode()+title.strip().split(' ')[0].encode()+'_UMI:'.encode()+sequence[:6].encode()+' '.encode()+title.strip().split(' ')[1].encode()+'\n'.encode()+sequence[(me_1_pos.start()+19):].encode()+'\n'.encode()+'+'.encode()+'\n'.encode()+quantily[(me_1_pos.start()+19):].encode()+'\n'.encode())
+        UMI_out.write('@'+title.strip().split(' ')[0]+'_UMI:'+sequence[:6]+' '+title.strip().split(' ')[1]+'\n'+sequence[(me_1_pos.start()+19):]+'\n'+'+'+'\n'+quantily[(me_1_pos.start()+19):]+'\n')
     elif me_15_pos and me_7_pos and (me_15_pos.start() - me_7_pos.start() == 8):
-        UMI_out.write('@'.encode()+title.strip().split(' ')[0].encode()+'_UMI:'.encode()+sequence[:6].encode()+' '.encode()+title.strip().split(' ')[1].encode()+'\n'.encode()+sequence[(me_15_pos.start()+5):].encode()+'\n'.encode()+'+'.encode()+'\n'.encode()+quantily[(me_15_pos.start()+5):].encode()+'\n'.encode())
+        UMI_out.write('@'+title.strip().split(' ')[0]+'_UMI:'+sequence[:6]+' '+title.strip().split(' ')[1]+'\n'+sequence[(me_15_pos.start()+5):]+'\n'+'+'+'\n'+quantily[(me_15_pos.start()+5):]+'\n')
 UMI_out.close()
 print('Done!')
