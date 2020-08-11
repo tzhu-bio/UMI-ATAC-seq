ad1a7e27519dd4632633573b6ce5088332d2c730
Tao Zhu <54537420+tzhu-bio@users.noreply.github.com>
2020-08-11 21:04:03 +0800

    Update paired.py

--- paired.py
+++ paired.py
@@ -5,7 +5,7 @@ from Bio.SeqIO.QualityIO import FastqGeneralIterator
 parser = argparse.ArgumentParser(description='Paired the read1 and read2 file.')
 parser.add_argument('umi_fr1', action = 'store', nargs = '?', type = str, default = sys.stdin, help = 'Input new FASTQ read1 produced by remove_me.py ')
 parser.add_argument('fr2', action = 'store', nargs = '?', type = str, default = sys.stdin, help = 'Input FASTQ read2 produced by umi_tools_extract.py')
-parser.add_argument('outfile',action = 'store', nargs = '?',type = argparse.FileType('wb'), default = sys.stdout,help = 'Output file name')
+parser.add_argument('outfile',action = 'store', nargs = '?',type = str, default = sys.stdout,help = 'Output file name')
 args = parser.parse_args()
 r1_lst=[]
 f1 = gzip.open(args.umi_fr1,"rt",encoding='utf-8')
